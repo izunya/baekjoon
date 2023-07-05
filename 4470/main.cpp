6da9412dccfdfb85569e29e4e307422031788b66
izunya <whdtj5615@naver.com>
2023-07-06 01:28:40 +0900

    4470 보강

--- 4470/main.cpp
+++ 4470/main.cpp
@@ -6,13 +6,11 @@ int main()
 {
     int a;
     cin >> a; 
-    char b[a];
-    char c;
-    for(int i = 0; i< a; i++){
-        cin >> c;
-        b[i] = c;
-    }
-    for(int i = 0; i< a; i++){
-        cout << i+1 << ". " << b[i] << "\n";
+    cin.ignore();
+    for(int i = 1; i<= a; i++){
+        string b;
+        getline(cin, b);
+        printf("%d. ", i);
+        cout << b << "\n";
     }
 }
