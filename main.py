2d88255ac7192588f050e895ef0802352cda9b3c
izunya <whdtj5615@naver.com>
2024-02-07 02:33:25 +0900

    1

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-a = int(input())
-
-for i in range(a):
-    b = int(input())
-    y = ""
-    if b <= 25:
-        y = "World Finals"
-    elif b <= 1000:
-        y = "Round 3"
-    elif b <= 4500:
-        y = "Round 2"
-    else:
-        y = "Round 1"
-    print(f"Case #{i+1}: {y}")
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break;
